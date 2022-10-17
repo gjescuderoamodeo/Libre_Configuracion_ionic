aaf961a648b2d00d32c9c4f3dd17c4eded15c867
gjescudero <gjescuderoamodeo@gmail.com>
2022-10-17 09:17:08 +0200

    Test AlertController

--- pruebasInterfaces/src/app/home/home.page.ts
+++ pruebasInterfaces/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  handlerMessage = '';
+  roleMessage = '';
 
   casado=true;
   estadoCivil:string="casado";
@@ -16,7 +20,100 @@ export class HomePage {
 
 
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+  /*async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }*/
+
+
+  //con 2 botones cancelar y aceptar
+  /*async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }*/
+
+
+  //con un formulario en el alert
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      //buttons: ['OK'],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: (data) => {
+            console.log(data);
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: (data) => {
+            console.log(data);
+            console.log(data.nombre);
+          },
+        },
+      ],
+      inputs: [
+        {
+          name:"nombre",
+          placeholder: 'Name',
+        },
+        {
+          name:"nick",
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          name:"edad",
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          name:"comentario",
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 
   cambiaSelect(event:any){
     console.log(this.frutaElegida)
