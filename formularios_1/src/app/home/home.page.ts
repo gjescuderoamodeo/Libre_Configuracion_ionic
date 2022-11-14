becd87af32a0f2f13eea21153185d1fb321c9c0f
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2022-11-14 13:53:03 +0100

    formulario1 V2 y V3

--- formularios_1/src/app/home/home.page.ts
+++ formularios_1/src/app/home/home.page.ts
@@ -15,10 +15,25 @@ export class HomePage implements OnInit{
   validations_form: FormGroup;
   genders: Array<string>;
   usuario:usuario = new usuario();
+  matching_passwords_group: FormGroup;
 
 
   constructor( public formBuilder: FormBuilder,
-    private navCtrl: NavController) {}
+    private navCtrl: NavController) {
+      this.matching_passwords_group = new FormGroup({
+        password: new FormControl('', Validators.compose([
+        Validators.maxLength(15),
+        Validators.minLength(8),
+        Validators.pattern('^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.,-]).*$'),
+        Validators.required
+        ])),
+        confirmPassword: new FormControl('', Validators.required)
+        }, (formGroup: FormGroup) => {
+        return this.confirmPassword(formGroup);
+        });
+    }
+
+    
 
 
     ngOnInit() {
@@ -28,12 +43,14 @@ export class HomePage implements OnInit{
       "Female"
       ];
       this.validations_form = this.formBuilder.group({
-      username: new FormControl('', Validators.compose([
-      Validators.maxLength(25),
-      Validators.minLength(5),
-      Validators.pattern('^[a-zA-Z]{1}[a-zA-Z0-9]+$'),
-      Validators.required
-      ])),
+        username: new FormControl('', Validators.compose([
+          this.validUsername,
+          Validators.maxLength(25),
+          Validators.minLength(5),
+          Validators.pattern('^[a-zA-Z]{1}[a-zA-Z0-9]+$'),
+          Validators.required
+          ])),
+          matching_passwords: this.matching_passwords_group,    
       name: new FormControl('', Validators.required),
       lastname: new FormControl('', Validators.required),
       email: new FormControl('', Validators.compose([
@@ -68,6 +85,21 @@ onSubmit(values){
   };
   this.navCtrl.navigateForward('/user', navigationExtras);
   }
+
+  validUsername(fc: FormControl){
+    if(fc.value.toLowerCase() === "abc123" || fc.value.toLowerCase() === "cba321"){
+            return ({validUsername: true});
+    } else {
+                 return (null);
+    }
+    }
+
+    confirmPassword(fg: FormGroup){
+      if(fg.controls['password'].value!=fg.controls['confirmPassword'].value)
+      return({confirmPassword:true});
+      else
+      return(null);
+      }
   
   
 }
