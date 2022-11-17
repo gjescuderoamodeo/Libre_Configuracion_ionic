4c52158e08f6312b284ff3d32179063c47e47f4b
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2022-11-17 09:17:28 +0100

    Practica 19 II
    (no funsiona)

--- practica19/src/app/home/home.page.ts
+++ practica19/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, ValidatorFn,FormGroup, Validators } from '@angular/forms';
 import { NavigationExtras } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import {alumno} from '../modelo/alumno';
@@ -11,26 +11,50 @@ import {alumno} from '../modelo/alumno';
 })
 export class HomePage implements OnInit{
 
-  validations_form: FormGroup | undefined;
+  validations_form: FormGroup;
+  sexos: Array<string>;
   alumno:alumno = new alumno();
 
   constructor( public formBuilder: FormBuilder,
-    private navCtrl: NavController) {}
+    private navCtrl: NavController) {
+      this.validations_form = this.formBuilder.group({
+        nombre: new FormControl('', Validators.required),
+        apellidos: new FormControl('', Validators.required),
+        edad: new FormControl('', Validators.required),
+        sexo: new FormControl('', Validators.required),
+        dni: new FormControl('', Validators.required),
+        apellidosPadre: new FormControl('', Validators.required)
+      }, { validators: [this.formularioNoValido()] });
+
+    }
 
   ngOnInit() {
-    this.validations_form = this.formBuilder.group({    
-    name: new FormControl('', Validators.required),
-    lastname: new FormControl('', Validators.required),
-    edad: new FormControl('', Validators.compose([
-    Validators.required,
-    Validators.pattern('^[a-zA-Z0-9_.+-]+[@]{1}[a-zA-Z0-9-]+[.]{1}[a-zA-Z]+$')
-    ]))
-    });
+    this.sexos = [
+      "Hombre",
+      "Mujer"
+    ];
+    }
+
+    
+
+    formularioNoValido(): ValidatorFn {
+      return (formGroup: FormGroup) => {
+        const nombre: string = formGroup.get('nombre').value;
+        const apellidos: string = formGroup.get('apellidos').value;
+  
+        if (false) return { isValid: false };
+        //en pruebas. Siempre devuelve true
+        return null;
+      };
     }
 
   onSubmit(values: any){
     console.log(values);
     this.navCtrl.navigateForward('/user');
   }  
+
+  getIntEdad(){
+    return(Number(this.validations_form.get('edad').value));
+  }
 
 }
