223c01a4188eb80e95629753a7bbd3aac29874a2
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-02-01 12:35:07 +0100

    previsionTiempo
    mejorado

---
+++ UT3/previsionTiempoClase/src/app/home/PrevisionPresentacion.ts
@@ -0,0 +1,9 @@
+export class PrevisionPresentacion {
+
+    public constructor(public fecha:string,
+        public periodo:string,
+        public iconoValue:string,
+        public descripcion:string){
+    }
+
+}
--- UT3/previsionTiempoClase/src/app/home/home.page.ts
+++ UT3/previsionTiempoClase/src/app/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { EstadoCielo, InterfacePrevisionDiariaMunicipio, InterfaceMunicipio } from '../modelo/Interfaces';
+import { EstadoCielo, InterfacePrevisionDiariaMunicipio, Temperatura, InterfaceMunicipio } from '../modelo/Interfaces';
 import { ApiServiceProvider } from '../providers/api-service/api-service';
+import { PrevisionPresentacion } from './PrevisionPresentacion';
 import { ActivatedRoute, NavigationExtras } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,7 @@ import { ActivatedRoute, NavigationExtras } from '@angular/router';
 export class HomePage implements OnInit {
 
   public estadosCielo: Array<EstadoCielo>=new Array();
+  public Temperatura: Temperatura;
   //public municipio:string='41091';
   public municipio:InterfaceMunicipio;
 
@@ -23,7 +26,7 @@ export class HomePage implements OnInit {
   });
   }
 
-  ngOnInit(): void {  
+  /*ngOnInit(): void {  
     this.apiService.getPrevisionDiariaMunicipio(this.municipio.codProvincia+this.municipio.codMunicipio)
       .then((data: InterfacePrevisionDiariaMunicipio[]) => {
         //previsión para hoy
@@ -44,7 +47,44 @@ export class HomePage implements OnInit {
 
       //
       //this.apiService.getMunicipios();
-  }
+  }*/
+
+  ngOnInit(): void {
+    this.apiService.getPrevisionDiariaMunicipio(this.municipio.codProvincia + this.municipio.codMunicipio)
+      .then((data: InterfacePrevisionDiariaMunicipio[]) => {
+        let fechaHoraActual = moment(new Date());
+        fechaHoraActual.locale('es');
+        let horaActual: number = fechaHoraActual.hour();
+        //previsión para hoy
+        //solo añado la previsión de los intervalos horarios superiores
+        //a la hora actual
+        this.Temperatura=data[0].prediccion.dia[0].temperatura;
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[3].periodo.substring(3, 5))) {
+          this.estadosCielo.push(data[0].prediccion.dia[0].estadoCielo[3]);
+        }
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[4].periodo.substring(3, 5))) {
+          this.estadosCielo.push(data[0].prediccion.dia[0].estadoCielo[4]); //intervalo de las 6 a las 12
+        }
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[5].periodo.substring(3, 5))) {
+          this.estadosCielo.push(data[0].prediccion.dia[0].estadoCielo[5]); //intervalo de las 12 a las 18
+        }
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[6].periodo.substring(3, 5))) {
+          this.estadosCielo.push(data[0].prediccion.dia[0].estadoCielo[6]); //intervalo de las 18 a las 24
+        }
+
+        console.log(data);
+
+        //previsión para mañana
+        this.estadosCielo.push(data[0].prediccion.dia[1].estadoCielo[3]); //intervalo de las 0 a las 6
+        this.estadosCielo.push(data[0].prediccion.dia[1].estadoCielo[4]); //intervalo de las 6 a las 12
+        this.estadosCielo.push(data[0].prediccion.dia[1].estadoCielo[5]); //intervalo de las 12 a las 18
+        this.estadosCielo.push(data[0].prediccion.dia[1].estadoCielo[6]); //intervalo de las 18 a las 24
+      })
+      .catch((error: string) => {
+        console.log(error);
+      });
+  }//end_ngOnInit
+
 
   
   }
